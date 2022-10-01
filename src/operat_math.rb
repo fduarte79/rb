e57be90e0a8aca955407edc7d4fa48ccb7a11611
Felipe <felipe.duarte79@protonmail.com>
2022-10-01 17:45:57 -0300

    Add files via upload

---
+++ src/operat_math.rb
@@ -0,0 +1,11 @@
+puts 'Type a number: '
+x = gets.to_f()
+puts 'Type another number: '
+y = gets.to_f()
+
+puts
+puts "Addition: #{y} + #{x} = #{y + x}"
+puts "Subtractation: #{y} - #{x} = #{y - x}"
+puts "Multiplication: #{y} * #{x} = #{y * x}"
+puts "Exponentiation: #{y} ** #{x} = #{y ** x}"
+puts "Modulus: #{y} % #{x} = #{y % x}"
