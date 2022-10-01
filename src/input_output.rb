bb3da6890551d3fce4cf5d7eb8a78a4beaf309f1
Felipe <felipe.duarte79@protonmail.com>
2022-10-01 15:52:40 -0300

    Add files via upload

---
+++ src/input_output.rb
@@ -0,0 +1,12 @@
+# input output
+
+puts 'Type your name: '
+name = gets.chomp()
+
+puts 'Operator + with string'  
+puts "Hello " + name + " !!!"
+
+puts
+
+puts 'Interpolation'
+puts "Hello #{name} !!!"
