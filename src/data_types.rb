587f7467b853a74540f144892b945febbaac621f
Felipe <felipe.duarte79@protonmail.com>
2022-10-01 15:30:43 -0300

    Add files via upload

---
+++ src/data_types.rb
@@ -0,0 +1,18 @@
+# types of data
+
+# integer
+puts 'Type a value between -9 and 9: '
+int = gets.to_i()
+puts "#{int} is #{int.class}"
+
+# float
+puts
+puts 'Type the value of pi: '
+float = gets.to_f()
+puts "#{float} is #{float.class}"
+
+# string
+puts
+puts 'Type a name: '
+string = gets()
+puts "#{string} is #{string.class}"
